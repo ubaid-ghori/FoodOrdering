0aa7c9681935f1ef2e3fb9c15f9e3a4bdeef9a15
ubaid ghori <ubaid.razaghori@gmail.com>
2024-10-05 16:02:54 +0500

    create a first component and implement it

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,44 +1,13 @@
-import Colors from "@/src/constants/Colors";
 import { StyleSheet, Text, View, Image } from "react-native";
-import products from "@/assets/data/products";
+import products from "../../../assets/data/products";
+import ProductListItem from "@/src/components/ProductListItem";
 
-const product = products[0];
-
-const productListItem=()=>{
-  return(
-    <View style={styles.container}>
-    <Image source={{ uri: product.image }} style={styles.image} />
-    <Text style={styles.title}>{product.name}</Text>
-    <Text style={styles.price}>${product.price}</Text>
-  </View>
-  )
-}
-
-export default function TabOneScreen() {
+export default function MenuScreen() {
   return (
    <View>
-    
+    <ProductListItem product={products[5]} />
    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "white",
-    padding: 10,
-    borderRadius: 20,
-  },
-  image: {
-    width: "100%",
-    aspectRatio: 1,
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: "600",
-  },
-  price: {
-    color: Colors.light.tint,
-    fontSize: 14,
-    fontWeight: "bold",
-  },
-});
+
---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,34 @@
+import Colors from "@/src/constants/Colors";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+const ProductListItem = ({ product }) => {
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: product.image }} style={styles.image} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+};
+
+export default ProductListItem;
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+  },
+  price: {
+    color: Colors.light.tint,
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+});
