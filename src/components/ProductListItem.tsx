c40caf7f061006946b9c65fe6bb14360dd274201
ubaid ghori <ubaid.razaghori@gmail.com>
2024-10-05 18:51:29 +0500

    Add a flatlist

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -2,16 +2,21 @@ import Colors from "@/constants/Colors";
 import { StyleSheet, Text, View, Image } from "react-native";
 import { Product } from "../types";
 
-export const defaultPizzaImage='https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png'
+export const defaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
 
 type ProductListItemProps = {
   product: Product;
-}
+};
 
-const ProductListItem = ({ product }:ProductListItemProps) => {
+const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: product.image || defaultPizzaImage  }} style={styles.image} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+        resizeMode="contain"
+      />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
@@ -24,6 +29,8 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     padding: 10,
     borderRadius: 20,
+    flex: 1,
+    maxWidth: "50%",
   },
   image: {
     width: "100%",
