f95e2df9dced8f5ddb75024801dbb6cbc20059e2
ubaid ghori <ubaid.razaghori@gmail.com>
2024-10-07 19:22:20 +0500

    shopping cart functionaly done

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -3,16 +3,19 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { useCart } from "@/providers/CartProvider";
 import CartListItem from "@/components/CartListItems";
+import Button from "@/components/Button";
 const CartScreen = () => {
-  const { items } = useCart();
+  const { items,total } = useCart();
 
   return (
-    <View>
+    <View style={{padding:10}}>
       <FlatList
         data={items}
         renderItem={({ item }) => <CartListItem cartItem={item} />}
         contentContainerStyle={{ padding:10,gap:10}}
       />
+      <Text style={{marginTop:10,fontWeight:"500",fontSize:20,}}>${total}</Text>
+      <Button  text="checkout"/>
       <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
     </View>
   );
