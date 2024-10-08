65c5a1ae4ce63294f3f7dc7e56e92ffeec25a4ac
ubaid ghori <ubaid.razaghori@gmail.com>
2024-10-08 12:27:06 +0500

    users order and deatils done

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -38,6 +38,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="orders"
         options={{
+          headerShown:false,
           title: 'Orders',
           tabBarIcon: ({ color }) => <TabBarIcon name="list" color={color} />,
         }}
