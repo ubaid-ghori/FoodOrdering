0079f344b3f4b75cdfbb0e2b087803c3a03f70d8
ubaid ghori <ubaid.razaghori@gmail.com>
2024-10-08 14:59:44 +0500

    install supabase

---
+++ src/lib/supabase.ts
@@ -0,0 +1,27 @@
+import 'react-native-url-polyfill/auto';
+import * as SecureStore from 'expo-secure-store';
+import { createClient } from '@supabase/supabase-js';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = 'https://fqfvojwcncbiqpclwvrv.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxZnZvandjbmNiaXFwY2x3dnJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgzODEyNjQsImV4cCI6MjA0Mzk1NzI2NH0.2Zx6MlWDn10bcbYuXcR-OBAkJkrZTrY-BU21jRbO71I';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
